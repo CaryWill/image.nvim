dda8858e8f6e91cb066cd25ae82aeb5ffee375fc
carywill2 <carywill.k1@gmail.com>
2023-12-24 20:48:27 +0800

    add working orgmode image display

---
+++ lua/image/integrations/org.lua
@@ -0,0 +1,55 @@
+local document = require("image/utils/document")
+
+local function findMatches(input)
+  local pattern = "%[%[([^%[%]]-)%]%]"
+  local matches = {}
+  for start_col, end_col, match in input:gmatch("()" .. pattern .. "()") do
+    table.insert(matches, { start_col, end_col, match })
+  end
+  return matches
+end
+
+local function findPatternOccurrences()
+  local total_lines = vim.fn.line("$")
+  local occurrences = {}
+  for start_row = 1, total_lines do
+    local line = vim.fn.getline(start_row)
+    local start_col = nil
+    local end_col = nil
+    local url = nil
+
+    local matches = findMatches(line)
+    local first_match = matches[1]
+    if first_match ~= nil then
+      start_col = first_match[1]
+      url = first_match[2]
+      end_col = first_match[3]
+    end
+
+    if start_col ~= nil then
+      local end_row = start_row
+      table.insert(occurrences, {
+        node = nil,
+        range = { start_row = start_row, start_col = start_col, end_row = end_row, end_col = end_col },
+        url = url,
+      })
+    end
+  end
+
+  return occurrences
+end
+
+return document.create_document_integration({
+  name = "org",
+  -- debug = true,
+  default_options = {
+    clear_in_insert_mode = false,
+    download_remote_images = true,
+    only_render_image_at_cursor = false,
+    filetypes = { "org" },
+  },
+  query_buffer_images = function(buffer)
+    local images = findPatternOccurrences()
+    return images
+  end,
+})
